f1f7ba55434a1ebab9ec1d3e2c5ffe72dfb0c123
ittonnoshio <86090538+ittonnoshio@users.noreply.github.com>
2021-07-14 19:06:26 +0900

    エラーメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :nickname, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
