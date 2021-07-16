27f57e6507bb642c682648be0a7d3a45f6d4c093
ittonnoshio <86090538+ittonnoshio@users.noreply.github.com>
2021-07-16 12:52:42 +0900

    ActiveHash導入と商品出品機能（画像以外）の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
