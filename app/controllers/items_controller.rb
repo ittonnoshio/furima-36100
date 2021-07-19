050f642f49a72b00b25bce1c8f0083fce52b467e
ittonnoshio <86090538+ittonnoshio@users.noreply.github.com>
2021-07-19 17:30:21 +0900

    itemsコントローラーのmove_to_indexアクションで、redirect_toのパスを変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
 
   def move_to_index
     item = Item.find(params[:id])
-    redirect_to root_path unless user_signed_in? && current_user.id == item.user_id
+    redirect_to new_user_session_path unless user_signed_in? && current_user.id == item.user_id
   end
 
   private
